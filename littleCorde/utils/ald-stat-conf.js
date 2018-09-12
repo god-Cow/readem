90f221daa8d9aaf4a9f5c69f219ef4b1329abc49
shiyinjun <13488716911@163.com>
2018-09-12 14:08:56 +0800

    shanchu

--- littleCorde/utils/ald-stat-conf.js
+++
@@ -1,5 +0,0 @@
-exports.app_key = "e4aa967b97e4ac1b0a2ca1b433fff1bb"; //请在此行填写从阿拉丁后台获取的appkey
-exports.getLocation = false; //默认不获取用户坐标位置
-exports.appid = ""; //用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.appsecret = "";//用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.defaultPath = 'pages/home/index';//小程序的默认首页, 用于分享时path为空时
